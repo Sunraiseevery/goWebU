2c35c80ca053d20bee7e82124bd23f0692ade786
Sunraiseevery <127868379+Sunraiseevery@users.noreply.github.com>
2025-08-22 18:31:45 +0800

    feat: open browser on startup

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os/exec"
+	"runtime"
+	"time"
 )
 
 func main() {
@@ -22,8 +26,30 @@ func main() {
 	}
 
 	srv := NewServer(db)
+
+	go func() {
+		// Allow the server a brief moment to start before opening the browser.
+		time.Sleep(200 * time.Millisecond)
+		openBrowser(fmt.Sprintf("http://localhost%s/", *addr))
+	}()
+
 	log.Printf("listening on %s", *addr)
 	if err := http.ListenAndServe(*addr, srv.routes()); err != nil {
 		log.Fatalf("server error: %v", err)
+	}
+}
+
+func openBrowser(url string) {
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "darwin":
+		cmd = exec.Command("open", url)
+	case "windows":
+		cmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", url)
+	default:
+		cmd = exec.Command("xdg-open", url)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Printf("failed to open browser: %v", err)
 	}
 }
